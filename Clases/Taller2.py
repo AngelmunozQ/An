02a079c7a306a55c33a23f6ba92c1838b2167622
Angel <angelmiliom54@gmail.com>
2021-02-16 19:53:47 -0500

    Avance

--- Clases/Taller2.py
+++ Clases/Taller2.py
@@ -1,12 +1,13 @@
-#x = int(input("dame un numero : "))
-#y = int(input(" dame otro numero : "))
-#if x>=5:
-  #  print (x,"mayor o igual que ",y)
-#elif x<=y:
- #   print(y,"mayor o igual que",x)
-#else:
-   # print("corrija")
-
+x = int(input("dame un numero : "))
+y = int(input(" dame otro numero : "))
+if x>=5:
+  print (x,"mayor o igual que ",y)
+elif x<=y:
+    print(y,"mayor o igual que",x)
+else:
+    print("corrija")
+    
+# TAMBIEN CON FUNCIONES #
 def mayoromenor (num1,num2):
     if num1>=num2:
         print("ok")
