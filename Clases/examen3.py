31f8d1f39189439b75494316f7bb3ec25355f31a
Angel <angelmiliom54@gmail.com>
2021-04-17 09:25:08 -0500

    Examen#3

---
+++ Clases/examen3.py
@@ -0,0 +1,54 @@
+#1
+"""def Calcu (num1,num2,num3):
+    Z=(num1*num2*num3)
+    f=(num1/num2/num3)
+    W=(num1**num2**num3)
+    print(f"{Z},{f},{W}")
+
+
+print("""
+#1. Multiplicacion,Potencia y Division. 
+#2.Salir
+""")
+
+Eleccion = (input("seleccione una opcion : ")) 
+
+if Eleccion == "1":
+    a = int(input("ingresa numero : "))
+    y = int(input("ingresa numero : "))
+    p = int(input("ingrese numero:  "))
+
+    Calcu(a,y,p)
+
+else:
+    (print("Adios"));"""
+
+#3
+""""def triangulo (r1,r2):
+    J=(r1*r2/2)
+    print(J)
+
+print("Ingresa la base y altura del triangulo para calcular el AREA.")
+i=float(input("Ingresa base: "))
+k=float(input("ingresa Altura: "))
+triangulo(i,k)"""
+
+#2
+"""def list ():
+    lista1=[1,3,4]
+    lista2=[3,4,3]
+    lista3=[3,2,1]
+    print(f"{lista1},{lista2},{lista3}");"""
+
+#4
+AB= [36,37,38,35,36,38]
+def encontrar ():
+    print("Elemento mayor", max(AB))
+    print("Elemento menor",min(AB))
+    suma=0
+    for i in AB:
+        suma+=i
+    promedio =suma/6
+    print(promedio)
+
+#5
